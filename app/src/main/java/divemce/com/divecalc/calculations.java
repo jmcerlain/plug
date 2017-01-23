54c0ec1d6505e9e1d28e9b4ac23a20c21e604bc0
splash9000 <splash9000@gmail.com>
2017-01-23 00:39:37 -0500

    alpha launch icon, calculations app bar menu

--- app/src/main/java/divemce/com/divecalc/calculations.java
+++ app/src/main/java/divemce/com/divecalc/calculations.java
@@ -3,6 +3,8 @@ package divemce.com.divecalc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class calculations extends AppCompatActivity {
@@ -35,6 +37,12 @@ public class calculations extends AppCompatActivity {
     public void showEad(View view) {
         Intent intent = new Intent(this, ead.class);
         startActivity(intent);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.calculations_menu,menu);
+        return true;
     }
 
 }
