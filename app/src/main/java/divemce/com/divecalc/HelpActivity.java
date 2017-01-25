1598c17b50f68ec8a3c09db5083a2994d3fe9eb8
splash9000 <splash9000@gmail.com>
2017-01-24 21:47:57 -0500

    helpactivity.java viewstub

--- app/src/main/java/divemce/com/divecalc/HelpActivity.java
+++ app/src/main/java/divemce/com/divecalc/HelpActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewStub;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -13,8 +14,13 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ViewStub stub = (ViewStub) findViewById(R.id.layout_stub);
+        stub.setLayoutResource(R.layout.content_help);
+        View inflated = stub.inflate();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
