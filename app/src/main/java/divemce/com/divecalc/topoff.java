1adb45fa33b99f3afec49fa9b89f0cb83e4f0533
splash9000 <splash9000@gmail.com>
2017-01-29 19:22:43 -0500

    topoff actionbar functionality

--- app/src/main/java/divemce/com/divecalc/topoff.java
+++ app/src/main/java/divemce/com/divecalc/topoff.java
@@ -1,7 +1,11 @@
 package divemce.com.divecalc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class topoff extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class topoff extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topoff);
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.calculations_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Intent intent = new Intent(this, about.class);
+                this.startActivity(intent);
+                return true;
+
+            case R.id.action_help:
+                Intent intentHelp = new Intent(this, HelpActivity.class);
+                intentHelp.putExtra("help_name","BestMix");
+                this.startActivity(intentHelp);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
