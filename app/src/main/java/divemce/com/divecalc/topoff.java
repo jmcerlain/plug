b9279c78c911f22631e6579a5dda7e67b35f5b5c
splash9000 <splash9000@gmail.com>
2017-01-31 00:25:22 -0500

    temp calcit topoff

--- app/src/main/java/divemce/com/divecalc/topoff.java
+++ app/src/main/java/divemce/com/divecalc/topoff.java
@@ -3,44 +3,89 @@ package divemce.com.divecalc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
+
+
+//float f = Float.parseFloat("25");
+//String s = Float.toString(25.0f);
+
 
 public class topoff extends AppCompatActivity implements View.OnClickListener {
 
-    private int depth = 60;
-    private double results = 33.0;
+    private double cur_mix_pct = 0;
+    private double results = 0;
 
     String strResults = new String();
     String strTmp = new String();
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topoff);
 
+
+
         // Setup listeners
 
         View calcButton = findViewById(R.id.calc_button);
         calcButton.setOnClickListener(this);
 
-  //      EditText e = (EditText) findViewById(R.id.depth_field);
-  //      e.addTextChangedListener(new bestmix.CustomTextWatcher(e));
+        EditText e = (EditText) findViewById(R.id.cur_o2_field);
+        e.addTextChangedListener(new topoff.CustomTextWatcher(e));
 
 
     }
+    private class CustomTextWatcher implements TextWatcher {
+        private EditText mEditText;
+        public CustomTextWatcher(EditText e) {
+            mEditText = e;     }
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            //ShowMsg();
+        }
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            ClearResults();
+        }
+        public void afterTextChanged(Editable s) {
+            //ShowMsg();
+        }
+    }
 
+    private void ClearResults() {
+        TextView t = (TextView) findViewById(R.id.result_field);
+        t.setText(" ");
+    }
 
     // temp dummy CalcIt
     private void CalcIt() {
-            strResults = "29.4";
-            strResults = strResults + " %";
+        EditText e = (EditText) findViewById(R.id.cur_o2_field);
+
+        strTmp = "0" + String.valueOf( e.getText());
+        //cur_mix_pct = Integer.parseInt(strTmp);
+
+        //NumberFormat nf = NumberFormat.getInstance();
+
+        //cur_mix_pct = nf.parse(strTmp);
+
+        cur_mix_pct = Float.parseFloat(strTmp);
+
+        results = cur_mix_pct + 11.1;
+
+
+        strResults= String.valueOf(results);
+
+       // strResults = "29.4";
+        //    strResults = strResults + " %";
+
+
             TextView t = (TextView) findViewById(R.id.result_field);
             t.setText(strResults);
         }
