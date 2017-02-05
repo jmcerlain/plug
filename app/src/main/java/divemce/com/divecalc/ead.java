d988e4ba380fdb5779cad784d4b1981cade71928
splash9000 <splash9000@gmail.com>
2017-02-04 23:30:19 -0500

    EAD wmsg ref string res

--- app/src/main/java/divemce/com/divecalc/ead.java
+++ app/src/main/java/divemce/com/divecalc/ead.java
@@ -208,7 +208,7 @@ public class ead extends AppCompatActivity implements OnClickListener {
 
             case R.id.calc_button:
                 //CalcIt();
-                
+
                 WarnMsg(getString(R.string.warn_msg_cur_top_psi));
                 break;
         }
