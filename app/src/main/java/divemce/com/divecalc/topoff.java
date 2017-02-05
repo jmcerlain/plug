6273527740db89d5ad848d3fbfb60e141b6d30c6
splash9000 <splash9000@gmail.com>
2017-02-05 14:17:01 -0500

    topoff validate all psi

--- app/src/main/java/divemce/com/divecalc/topoff.java
+++ app/src/main/java/divemce/com/divecalc/topoff.java
@@ -7,12 +7,16 @@ import android.text.Editable;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -128,8 +132,21 @@ public class topoff extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
+    private void WarnMsg(String wmsg)
+    {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout,
+                (ViewGroup) findViewById(R.id.toast_layout_root));
 
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText(wmsg);
 
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 4);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
 
     private void ClearResults() {
         TextView t = (TextView) findViewById(R.id.result_field);
@@ -138,6 +155,13 @@ public class topoff extends AppCompatActivity implements View.OnClickListener {
 
     // temp dummy CalcIt
     private void CalcIt() {
+		boolean goodInput = true;
+		
+		
+		// get values from fields
+		
+		
+		
         //cur_mix_pct = Integer.parseInt(strTmp);
         //NumberFormat nf = NumberFormat.getInstance();
         //cur_mix_pct = nf.parse(strTmp);
@@ -166,19 +190,48 @@ public class topoff extends AppCompatActivity implements View.OnClickListener {
         top_mix_psi = Integer.parseInt(strTmp);
 
 
+        // validate values
+
+		if (cur_mix_psi < 1)
+        {
+            goodInput = false;
+			WarnMsg(getString(R.string.warn_msg_cur_psi));
+        }
 
 
-        //results = cur_mix_pct + top_mix_pct + cur_mix_psi + top_mix_psi+ 0.1;
-        results = cur_mix_pct + 2.2;
+        if ((top_mix_psi < 1) & (goodInput))
+        {
+            goodInput = false;
+			WarnMsg(getString(R.string.warn_msg_top_psi));
+        }
+		
+		if ((top_mix_psi < cur_mix_psi) & (goodInput))
+        {
+            goodInput = false;
+			WarnMsg(getString(R.string.warn_msg_cur_top_psi));
+        }
 
-        strResults= String.valueOf(results);
+		
 
-       // strResults = "29.4";
-        //    strResults = strResults + " %";
+		if (goodInput)
+        {		
+			//results = cur_mix_pct + top_mix_pct + cur_mix_psi + top_mix_psi+ 0.1;
+			results = cur_mix_pct + 2.2;
+
+			strResults= String.valueOf(results);
+
+			// strResults = "29.4";
+			//    strResults = strResults + " %";
 
 
-            TextView t = (TextView) findViewById(R.id.result_field);
-            t.setText(strResults);
+			TextView t = (TextView) findViewById(R.id.result_field);
+			t.setText(strResults);			
+		}
+			
+			
+			
+			
+			
         }
 
 
